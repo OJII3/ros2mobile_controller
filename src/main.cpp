68ef63a3909259361293cb46d68ae046888a40c5
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-08-29 20:06:50 +0900

    [add]: new class udpclient

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,8 @@
 #include "ros2mobile_controller.hpp"
-#include <rclcpp/executors.hpp>
-
-using namespace std;
+#include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
-  auto node = make_shared<ROS2MobileController>();
-
-  try {
-    auto timer = node->create_wall_timer(10ms, [&]() { node->update(); });
-
-    while (rclcpp::ok()) {
-      rclcpp::spin_some(node);
-    }
-
-  } catch (...) {
-    RCLCPP_INFO_STREAM(node->get_logger(), "Exception thrown. Exiting...");
-  }
   rclcpp::shutdown();
+  return 0;
 }
--- src/ros2mobile_controller.hpp
+++ src/ros2mobile_controller.hpp
@@ -12,7 +12,6 @@
 #include <sys/types.h>
 #include <vector>
 
-// TODO: create UDPClient class and use it in this class
 class ROS2MobileController : public rclcpp::Node {
 public:
   enum ConnectionState {
@@ -24,30 +23,7 @@ public:
   ROS2MobileController();
   ~ROS2MobileController();
   bool connect();
-  void start();  // start timtask
   void update(); // timtask
 
 private:
-  void send(const std::vector<int8_t> &buffer);
-  std::vector<int8_t> receive();
-  void topicCallback(const ros2usb_msgs::msg::USBPacket &msg);
-  std::vector<int8_t> rosmsgToBytes(const ros2usb_msgs::msg::USBPacket &msg);
-  void initSocket();
-  int socket_fd;
-  struct sockaddr_in local_addr;
-  struct sockaddr_in remote_addr;
-  socklen_t local_addr_len = sizeof(local_addr);
-  socklen_t remote_addr_len = sizeof(remote_addr);
-  const std::string local_address = "0.0.0.0";
-  std::string remote_address = "";
-  int local_port = 10000;
-  int remote_port = 10001;
-  int connectionFailCount = 0;
-  std::vector<int8_t> send_buffer;
-  std::vector<int8_t> receive_buffer;
-  const int maxConnectionFailCount = 10;
-  rclcpp::Subscription<ros2usb_msgs::msg::USBPacket>::SharedPtr subscription_;
-  rclcpp::Publisher<ros2usb_msgs::msg::USBPacket>::SharedPtr publisher_;
-  const std::string ping_message = "ping-robot";
-  const std::string sub_topic = "ros2micon";
 };
