df711362a73d3e12e2afe4e59db3c2bbea1b3a29
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-09-01 12:45:58 +0900

    [update]: not working?

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@ int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<ROS2MobileController>();
   constexpr auto period = std::chrono::milliseconds(10);
-  auto timer = node->create_wall_timer(period, [&]() -> void {
-    RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"), "Hello, world!");
-  });
+  auto timer =
+      node->create_wall_timer(period, [&]() -> void { node->update(); });
+  rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
--- src/ros2mobile_controller.hpp
+++ src/ros2mobile_controller.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
-#include <algorithm>
-#include <cstddef>
+#include <asio.hpp>
+#include <rclcpp/logging.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <ros2usb_msgs/msg/usb_packet.hpp>
-#include <vector>
-
-#include "udp_client/udp_client.hpp"
 
 class ROS2MobileController : public rclcpp::Node {
  public:
@@ -22,15 +19,25 @@ class ROS2MobileController : public rclcpp::Node {
   void update();  // timtask
 
  private:
-  static constexpr size_t rosmsg_size = 1024;
-  static constexpr uint16_t local_port = 10000;
+  void onReceive(asio::error_code error, size_t bytes_transferred);
+  static constexpr size_t data_size_default = 1024;
+  static constexpr size_t qos = 1024;
+  static constexpr uint16_t local_receive_port = 10000;
   static constexpr uint16_t remote_port = 10001;
+  static const int connection_timeout_ms = 3000;
   ConnectionState connection_state_ = Disconnected;
-  std::string ping_message_ = "ping-rur";
-  UDPClient udp_client_;
-  std::string local_ip_ = "0.0.0.0";
-  std::string remote_ip_;
+  asio::io_service io_service_;
+  asio::ip::udp::endpoint remote_endpoint_;
+  asio::ip::udp::socket receive_socket_;
+  asio::ip::udp::socket send_socket_;
+  std::string ping_message_ = "ping-robot";
+  std::vector<uint8_t> send_buffer_;
+  std::vector<uint8_t> receive_buffer_;
+  std::chrono::steady_clock::time_point last_received_time_;
+  std::string subscription_topic_ = "micon2ros";
   rclcpp::Publisher<ros2usb_msgs::msg::USBPacket>::SharedPtr publisher_;
+  rclcpp::Subscription<ros2usb_msgs::msg::USBPacket>::SharedPtr subscription_;
+  void subscriptionCallback(const ros2usb_msgs::msg::USBPacket &msg);
 
   void connect();
 };
